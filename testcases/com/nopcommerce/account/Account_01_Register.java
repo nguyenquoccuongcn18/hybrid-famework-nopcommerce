d13a39ae10ca72976c382dda475dbfda9c29a1bc
nguyenquoccuongcn18 <nguyenquoccuongcn18@gmail.com>
2024-05-03 23:31:37 +0700

    add comit

---
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,42 @@
+package com.nopcommerce.account;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+public class Account_01_Register {
+	
+	@BeforeClass
+	public void beforeClass() {
+		
+		
+	}
+	
+	@Test
+	public void Register_01_Emty_Data() {
+		
+		
+	}
+	public void Register_02_Invalid_Email() {
+		
+		
+	}
+	public void Register_03_Invalid_Password() {
+		
+		
+	}
+	public void Register_04_Invalid_Confirm_Password() {
+		
+		
+	}
+	public void Register_05_Sucess() {
+		
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		
+		
+	}
+	
+}
